5622cb9bd829cfbe99923068754a218bbb6af352
grngxd <36968271+grngxd@users.noreply.github.com>
2024-11-25 22:05:31 +0000

    make example plugin use preact

--- packages/example/index.tsx
+++ packages/example/index.tsx
@@ -10,10 +10,19 @@ const {
     removeCustomElement,
 } = veil.settings;
 
+const {
+    renderPreactInReact
+} = veil.ui.bridge;
+
+const {
+    Text,
+} = veil.ui.components
+
 export const init = () => {
     log("Example Plugin Initialized");
 
     addCustomElement({
+        element: renderPreactInReact(() => (<Text>Veil is cool</Text>)),
         section: "veiliscool",
         searchableTitles: ["section"],
         label: "this is a section",
