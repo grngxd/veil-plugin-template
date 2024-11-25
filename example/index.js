6c4ca17e7a60e8410020a5a5255fac2ec04c59f3
grngxd <grngxd@users.noreply.github.com>
2024-11-25 21:51:56 +0000

    deploy: 2fb6c572b87f4581465243dd8b31fe58dc8d0018

--- example/index.js
+++ example/index.js
@@ -1,25 +1,26 @@
-"use strict";
-(() => {
-  // packages/example/index.ts
-  var {
-    log
-  } = veil.util;
-  var {
-    addCustomElement,
-    removeCustomElement
-  } = veil.settings;
-  var init = () => {
-    log("Example Plugin Initialized");
-    addCustomElement({
-      section: "veiliscool",
-      searchableTitles: ["section"],
-      label: "this is a section"
-    });
-  };
-  var unload = () => {
-    log("Example Plugin Unloaded");
-    removeCustomElement({
-      section: "veiliscool"
-    });
-  };
-})();
+// packages/example/index.ts
+var {
+  log
+} = veil.util;
+var {
+  addCustomElement,
+  removeCustomElement
+} = veil.settings;
+var init = () => {
+  log("Example Plugin Initialized");
+  addCustomElement({
+    section: "veiliscool",
+    searchableTitles: ["section"],
+    label: "this is a section"
+  });
+};
+var unload = () => {
+  log("Example Plugin Unloaded");
+  removeCustomElement({
+    section: "veiliscool"
+  });
+};
+export {
+  init,
+  unload
+};
